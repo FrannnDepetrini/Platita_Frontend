772c4c792eeb72409ca80d2ba9bde1200b3e1d0b
Francisco Palena <palenafrancisco@gmail.com>
2025-04-11 18:31:20 -0300

    Card con atributos y truncamiento de texto

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import "./App.css";
 import JobCard from "./components/jobCard/JobCard";
 
+const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor, nisl eget ultricies tincidunt, nisl nisl aliquam nisl, eget ultricies nisl nisl eget nisl. Nullam auctor, nisl eget ultricies tincidunt, nisl nisl aliquam nisl, eget ultricies nisl nisl eget nisl. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+
+
 function App() {
   return (
     <div>
-      <JobCard />
+      <JobCard description={text} />
     </div>
   );
 }
