8cc8130c4190738b6f2ac70ec7d541c687ab3db2
Depeex <fdepeex@gmail.com>
2025-04-22 12:00:17 -0300

    Arreglo del problema con la clase Footer_Container

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import "./Footer.css";
 
 const Footer = () => {
   return (
-    <div id="footer_container" className="footer_container">
+    <div className="footer_container">
       <h3>© 2024 Platita. Designed by UTN students.</h3>
     </div>
   );
