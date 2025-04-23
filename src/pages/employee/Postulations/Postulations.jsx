b9d543a7bbb71559593c444905ac8c1795e4ab4c
Depeex <fdepeex@gmail.com>
2025-04-23 16:21:17 -0300

    Hecha la vista de Postulaciones

--- src/pages/employee/Postulations/Postulations.jsx
+++ src/pages/employee/Postulations/Postulations.jsx
@@ -1,9 +1,150 @@
+import "./Postulations.css";
+import { FaTrashAlt } from "../../../utils/icons/icons";
+import UseCategoryIcon from "../../../customHooks/UseCategoryIcon";
+import { useState } from "react";
+const postulationsApi = [
+  {
+    id: 1,
+    trabajo: "Levantar un tapial",
+    empleador: "Fulano Detal",
+    categoria: "Construction",
+    estado: "Pendiente",
+    fechaPostulacion: "07/04/25",
+  },
+  {
+    id: 2,
+    trabajo: "Levantar un tapial 2",
+    empleador: "Fulano Detal",
+    categoria: "Construction",
+    estado: "Aceptado",
+    fechaPostulacion: "07/04/25",
+  },
+  {
+    id: 3,
+    trabajo: "Levantar un tapial 3",
+    empleador: "Fulano Detal",
+    categoria: "Construction",
+    estado: "Finalizado ",
+    fechaPostulacion: "07/04/25",
+  },
+  {
+    id: 4,
+    trabajo: "Levantar un tapial 4",
+    empleador: "Fulano Detal",
+    categoria: "Construction",
+    estado: "Pendiente",
+    fechaPostulacion: "07/04/25",
+  },
+  {
+    id: 5,
+    trabajo: "Levantar un tapial 5",
+    empleador: "Fulano Detal",
+    categoria: "Construction",
+    estado: "Aceptado",
+    fechaPostulacion: "07/04/25",
+  },
+  {
+    id: 6,
+    trabajo: "Levantar un tapial 6",
+    empleador: "Fulano Detal",
+    categoria: "Construction",
+    estado: "Finalizado ",
+    fechaPostulacion: "07/04/25",
+  },
+  {
+    id: 7,
+    trabajo: "Levantar un tapial 7",
+    empleador: "Fulano Detal",
+    categoria: "Construction",
+    estado: "Pendiente",
+    fechaPostulacion: "07/04/25",
+  },
+  {
+    id: 8,
+    trabajo: "Levantar un tapial 8",
+    empleador: "Fulano Detal",
+    categoria: "Construction",
+    estado: "Aceptado",
+    fechaPostulacion: "07/04/25",
+  },
+  {
+    id: 9,
+    trabajo: "Levantar un tapial 9",
+    empleador: "Fulano Detal",
+    categoria: "Construction",
+    estado: "Finalizado ",
+    fechaPostulacion: "07/04/25",
+  },
+];
+
 export default function Postulations() {
-    return (
-      <div>
-        <h1>Postulaciones</h1>
-        <p>Lista de trabajos a los que te postulaste.</p>
+  const [postulations, setPostulations] = useState(postulationsApi);
+
+  const handleDeletePs = (id) => {
+    setPostulations(postulations.filter((ps) => ps.id !== id));
+  };
+
+  const postMapped = () => {
+    return postulations.map((ps) => {
+      const CategoryIcon = UseCategoryIcon(ps.categoria);
+      return (
+        <tr>
+          <td>{ps.trabajo} </td>
+          {/* Ese onClick debera enviarte al perfil de ese usuario */}
+          <td onClick={null} className="td_empleador">
+            <h4>{ps.empleador}</h4>
+          </td>
+          <td>
+            <CategoryIcon className="category_icon" />
+          </td>
+          <td>
+            <div
+              className={`td_state ${
+                ps.estado == "Pendiente"
+                  ? "Pending"
+                  : ps.estado == "Aceptado"
+                  ? "Accepted"
+                  : "Finished"
+              }`}
+            >
+              {ps.estado}
+            </div>
+          </td>
+          <td>{ps.fechaPostulacion} </td>
+          <td>
+            <FaTrashAlt
+              onClick={() => handleDeletePs(ps.id)}
+              className="delete_icon"
+            />
+          </td>
+        </tr>
+      );
+    });
+  };
+
+  return (
+    <div className="postulations_container">
+      <div className="table_container">
+        <table className="table_postulations">
+          <thead>
+            <tr>
+              <th>Trabajo</th>
+              <th>Empleador</th>
+              <th>Categoria</th>
+              <th>Estado</th>
+              <th>
+                Fecha de <br />
+                postulacion
+              </th>
+              <th>Acciones</th>
+            </tr>
+          </thead>
+          <tbody>{postMapped()}</tbody>
+          <tfoot>
+            <td aria-colspan={6}></td>
+          </tfoot>
+        </table>
       </div>
-    );
-  }
-  
+    </div>
+  );
+}
