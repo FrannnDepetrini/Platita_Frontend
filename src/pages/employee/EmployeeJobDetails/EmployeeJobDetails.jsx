4f99a0db8af35c67d695525d8355d3e7d6a82eb1
patriciogazzera <patriciogazzera7@gmail.com>
2025-05-08 13:57:15 -0300

    Creacion del contenedor del job

--- src/pages/employee/EmployeeJobDetails/EmployeeJobDetails.jsx
+++ src/pages/employee/EmployeeJobDetails/EmployeeJobDetails.jsx
@@ -1,12 +1,19 @@
 import { useParams } from "react-router-dom";
+import "./EmployeeJobDetails.css"
 
 export default function EmployerJobDetails() {
+
   const { id } = useParams();
 
   return (
     <div>
-      <h1>Detalles del Trabajo (Empleador)</h1>
-      <p>Viendo detalles del trabajo publicado con ID: {id}</p>
+      <div className="title-text">
+      <h1><span className="grey-text">Inicio </span><bold>/ Detalle del trabajo</bold></h1>
+      </div>
+      <div className="job-container-box">
+        <p>job</p>
+        <div className="box-separator"><hr color="#fbbd08"></hr></div>
+      </div>
     </div>
   );
 }
