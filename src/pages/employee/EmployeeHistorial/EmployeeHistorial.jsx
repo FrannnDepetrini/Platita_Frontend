e9d72ec9c29ed038f5cef02351c6fffd28bb4a8f
Depeex <fdepeex@gmail.com>
2025-05-16 21:28:49 -0300

    Implementada la api de magic lops y arreglados los estilos

--- src/pages/employee/EmployeeHistorial/EmployeeHistorial.jsx
+++ src/pages/employee/EmployeeHistorial/EmployeeHistorial.jsx
@@ -1,133 +1,21 @@
-// import React, { useEffect, useState } from "react";
 import UseCategoryIcon from "../../../customHooks/UseCategoryIcon";
-import { FaStar, FaStarHalf } from "react-icons/fa";
+import {
+  IoIosStar,
+  IoIosStarHalf,
+  IoIosStarOutline,
+} from "../../../utils/icons/icons";
 import "./EmployeeHistorial.css";
-
-// export function EmployeeHistorial() {
-//   const [jobs, setJobs] = useState([]);
-
-//   useEffect(() => {
-//     setJobs([
-//       {
-//         id: 1,
-//         titulo: "Levantar un tapial",
-//         empleador: "Fulano Detal",
-//         categoria: "Construction",
-//         fecha: "07/04/25",
-//         reseña: 5,
-//         tiempoExpirado: false,
-//       },
-//       {
-//         id: 2,
-//         titulo: "Levantar un tapial",
-//         empleador: "Fulano Detal",
-//         categoria: "Construction",
-//         fecha: "07/04/25",
-//         reseña: 3,
-//         tiempoExpirado: false,
-//       },
-//       {
-//         id: 3,
-//         titulo: "Levantar un tapial",
-//         empleador: "Fulano Detal",
-//         categoria: "Construction",
-//         fecha: "07/04/25",
-//         reseña: null,
-//         tiempoExpirado: false,
-//       },
-//       {
-//         id: 4,
-//         titulo: "Levantar un tapial",
-//         empleador: "Fulano Detal",
-//         categoria: "Construction",
-//         fecha: "07/04/25",
-//         reseña: null,
-//         tiempoExpirado: true,
-//       },
-//     ]);
-//   }, []);
-
-//   const handleReview = (job) => {
-//     if (job.reseña !== null) {
-//       return (
-//         <div className="stars">
-//           {[1, 2, 3, 4, 5].map((n) => (
-//             <FaStar key={n} className={n <= job.reseña ? "" : "inactive"} />
-//           ))}
-//         </div>
-//       );
-//     } else if (!job.tiempoExpirado) {
-//       return <span className="comment-link">Dejar una reseña</span>;
-//     } else if (job.tiempoExpirado) {
-//       return <span className="expired">El tiempo expiró</span>;
-//     }
-//   };
-
-//   return (
-//     <div className="employee-historial">
-//       <div className="breadcrumb">
-//         Inicio / <span className="current-page">Historial</span>
-//       </div>
-
-//       <div className="table-container">
-//         <table>
-//           <thead>
-//             <tr>
-//               <th>Trabajo</th>
-//               <th>Empleador</th>
-//               <th>Categoría</th>
-//               <th>Fecha de finalización</th>
-//               <th>Reseña</th>
-//             </tr>
-//           </thead>
-//           <tbody>
-//             {jobs.map((job) => {
-//               const Icon = UseCategoryIcon(job.categoria);
-//               return (
-//                 <tr key={job.id}>
-//                   <td className="job-title">{job.titulo}</td>
-//                   <td>{job.empleador}</td>
-//                   <td className="icon">{Icon && <Icon />}</td>
-//                   <td>{job.fecha}</td>
-//                   <td>{handleReview(job)}</td>
-//                 </tr>
-//               );
-//             })}
-//           </tbody>
-//         </table>
-//       </div>
-//     </div>
-//   );
-// }
-
-// import "./Postulations.css";
 import { FaTrashAlt } from "../../../utils/icons/icons";
-
 import { useEffect, useState } from "react";
 
 export default function EmployeeHistorial() {
   const [jobs, setJobs] = useState([]);
   const [loading, setLoading] = useState(true);
 
-  // const jobsMapped = () => {
-  //   return jobs.map((job) => {
-  //             const Icon = UseCategoryIcon(job.categoria);
-  //             return (
-  //               <tr key={job.id}>
-  //                 <td className="job-title">{job.titulo}</td>
-  //                 <td>{job.empleador}</td>
-  //                 <td className="icon">{Icon && <Icon />}</td>
-  //                 <td>{job.fecha}</td>
-  //                 <td>{handleReview(job)}</td>
-  //               </tr>
-  //             );
-  //           })}
-  // }
-
   const fetchData = async () => {
     try {
       const response = await fetch(
-        "https://magicloops.dev/api/loop/37a05862-b884-4529-9746-e40fc2109420/run?count=3"
+        "https://magicloops.dev/api/loop/37a05862-b884-4529-9746-e40fc2109420/run?count=4"
       );
       if (!response.ok) throw new Error("Sucedio un error inesperado");
       const data = await response.json();
@@ -142,28 +30,37 @@ export default function EmployeeHistorial() {
     fetchData();
   }, []);
 
-  const handleReview = (job) => {
-    if (job.reseña !== null) {
-      return (
-        <div className="stars">
-          {[1, 2, 3, 4, 5].map((n) => (
-            <FaStar key={n} className={n <= job.reseña ? "" : "inactive"} />
-          ))}
-        </div>
-      );
-    } else if (!job.tiempoExpirado) {
-      return <span className="comment-link">Dejar una reseña</span>;
-    } else if (job.tiempoExpirado) {
+  const starsCalculated = (average, expirationDate) => {
+    const today = new Date();
+    const dateMapped = new Date(expirationDate);
+    const isExpired = dateMapped < today;
+    var stars = [];
+
+    if (average == 0 && isExpired) {
       return <span className="expired">El tiempo expiró</span>;
+    } else if (average == 0 && !isExpired) {
+      return <span className="comment-link">Dejar una reseña</span>;
+    } else {
+      for (var i = 1; i < 6; i++) {
+        if (average >= i) {
+          stars.push(<IoIosStar className="star" />);
+        } else if (average >= i - 0.5) {
+          stars.push(<IoIosStarHalf className="star" />);
+        } else {
+          stars.push(<IoIosStarOutline className="star" />);
+        }
+      }
+
+      return stars;
     }
   };
 
   const handleDeleteJob = (id) => {
-    setJobs(jobs.filter((job) => job !== id));
+    setJobs(jobs.filter((job) => job.id !== id));
+    console.log("elimine");
   };
 
   const jobsMapped = () => {
-    console.log(jobs);
     if (jobs.length == 0) {
       <td colspan={6}>No tienes trabajos aún!</td>;
     } else {
@@ -180,9 +77,9 @@ export default function EmployeeHistorial() {
               <CategoryIcon className="category_icon" />
             </td>
             <td>
-              <div className="td_date">{job.date}</div>
+              <div>{job.date}</div>
             </td>
-            <td>{handleReview(job)} </td>
+            <td>{starsCalculated(job.review, job.expirationDate)} </td>
             <td>
               <FaTrashAlt
                 onClick={() => handleDeleteJob(job.id)}
@@ -196,9 +93,9 @@ export default function EmployeeHistorial() {
   };
 
   return (
-    <div className="postulations_container">
+    <div className="job_container">
       <div className="table_container">
-        <table className="table_postulations">
+        <table className="table_job">
           <thead>
             <tr>
               <th>Trabajo</th>
